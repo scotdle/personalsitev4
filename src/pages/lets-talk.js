02023e61513d913b0011edea90548328d6c38831
Scott Caudle <scottcaudlewebsite@gmail.com>
2019-08-02 01:00:17 -0400

    retrying updated netlify form

--- src/pages/lets-talk.js
+++ src/pages/lets-talk.js
@@ -24,22 +24,21 @@ class NameForm extends React.Component {
             <Layout>
 
                 <h1 className={'nameHeader'}>I hope we can talk soon {this.state.name}.</h1>
-                <Form className={'form'} name='contact' data-netlify='true' data-netlify-honeypot="bot-field"
-
+                <Form className={'form'} name='contact' data-netlify='true' data-netlify-honeypot='bot-field'
                       method='post'>
                     <Form.Group controlId="name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control size={'lg'} type="text" placeholder="whats your name?"
+                        <Form.Control size={'lg'} type="text" name={'yourName'} placeholder="whats your name?"
                                       onChange={this.handleChange}/>
                     </Form.Group>
                     <Form.Group controlId="emailAddress">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="enter your email" size={'lg'}/>
+                        <Form.Control type="email" name={'yourEmail'} placeholder="enter your email" size={'lg'}/>
                     </Form.Group>
                     <Form.Group controlId="message">
                         <Form.Label>Message</Form.Label>
-                        <textarea className={"form-control"} rows={"7"} size={'lg'}></textarea>
-                        <input type="hidden" name="bot-field"/>
+                        <textarea className={"form-control"} name={'yourMessage'} rows={"7"} size={'lg'}></textarea>
+                        <input type="hidden" name="bot-field" value={'contact'}/>
                     </Form.Group>
                 </Form>
                 <p>
