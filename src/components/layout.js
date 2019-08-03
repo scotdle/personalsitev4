eceb0afad02a3f0049dcf25ef8a23212cc35156f
Scott Caudle <scottcaudlewebsite@gmail.com>
2019-08-03 02:25:15 -0400

    getting rid of style component for now.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-import './layout.css';
 
 import React from 'react';
 import { Link } from 'gatsby';
@@ -6,10 +5,6 @@ import Nav from '../components/nav/nav'
 import Footer from '../components/footer/footer'
 import {Container} from 'react-bootstrap'
 
-const noPadding = {
-    padding: 0,
-    margin: 0
-};
 
 const  Layout = ({children}) => (
     <div>
